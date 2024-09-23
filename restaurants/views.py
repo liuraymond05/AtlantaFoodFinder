7955d413cad10381916db9288807679ecfc4e3a2
jordyn626 <jordynh626@gmail.com>
2024-09-23 15:48:09 -0400

    Created a user login page that directs to the geolocation finder for the website
    Completed the user login page to enter the website.

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from django.contrib import auth, messages
+from .forms import LoginForm
 import json
 from django.core.serializers import serialize
 
@@ -11,3 +13,23 @@ def map_view(request):
         list(restaurants.values('name', 'latitude', 'longitude', 'description'))
     )
     return render(request, 'restaurants/map.html', {'restaurants': restaurants})
+
+def index_view(request):
+    return render(request, 'restaurants/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+    return render(request, 'restaurants/login.html', {'form': LoginForm})
+
+def logout_view(request):
+    auth.logout(request)
+    messages.info(request, 'You have successfully logged out!')
+    return redirect('/')
